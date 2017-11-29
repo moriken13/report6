1e291ae973a1efca24c2e232d7a68f9392c93397
Kenta Mori <e175713@ie.u-ryukyu.ac.jp>
2017-11-29 14:59:11 +0900

    First commit

---
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175713/LivingThing.java
@@ -0,0 +1,54 @@
+package jp.ac.uryukyu.ie.e175713;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int maximumHP, int attack){
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if (dead == false){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int damage) {
+        this.hitPoint -= damage;
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
+

339a4f4407004892fe16fa2893d95a6dd56db524
moriken13 <e175713@ie.u-ryukyu.ac.jp>
2018-01-11 19:36:21 +0900

    First commit

---
+++ IdeaProjects/report6_1/src/main/java/jp/ac/uryukyu/ie/e175713/Boss.java
@@ -0,0 +1,28 @@
+package jp.ac.uryukyu.ie.e175713;
+
+public class Boss extends Human{
+
+    boolean death;
+    public Boss(){
+        super.setName("茅場晶彦");
+        super.setHp(50);
+        super.setAttackPower(7);
+        this.death = false;
+    }
+
+    public boolean deathEvent(){ return death; }
+
+    public void attack(Hero o){
+        int damage = (int)(Math.random() * getAttackPower());
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！\n",getName(),o.getName(),damage);
+        o.scar(damage);
+    }
+
+    public void scar(int damage){
+        int remainder = getHp() - damage;
+        if( remainder < 0 ) {
+            death = true;
+            System.out.printf("%sは力尽きてしまった。\n", getName());
+        }
+    }
+}
---
+++ IdeaProjects/report6_1/src/main/java/jp/ac/uryukyu/ie/e175713/Main.java
@@ -0,0 +1,20 @@
+
+package jp.ac.uryukyu.ie.e175713;
+
+public class Main {
+    public static void main(String[] args){
+        Hero hero = new Hero();
+        Boss boss = new Boss();
+
+        System.out.printf("%s vs. %s\n", hero.getName(), boss.getName());
+
+        int turn = 0;
+        while(!hero.deathEvent() && !boss.deathEvent()){
+            turn++;
+            System.out.printf("%dターン目開始！\n", turn);
+            hero.attack(boss);
+            boss.attack(hero);
+        }
+        System.out.println("戦闘終了");
+    }
+}
